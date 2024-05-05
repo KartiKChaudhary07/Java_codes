773836f570c136c590c0ff4032b45b901be82df6
Kartik Chaudhary <117715537+KartiKChaudhary07@users.noreply.github.com>
2024-05-05 17:53:11 +0530

    Create Graph01.java

---
+++ Graph01.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Graph01 {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+    }
+
+    public static void bfs(ArrayList<Edge>[] graph) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[graph.length];
+        q.add(0);
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            if (!vis[curr]) {
+                System.out.print(curr + " ");
+                vis[curr] = true;
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int v = 5;
+        ArrayList<Edge>[] graph = new ArrayList[v];
+        createGraph(graph);
+        graph[0].add(new Edge(0, 1, 5));
+        graph[1].add(new Edge(1, 0, 5));
+        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 3));
+        graph[2].add(new Edge(2, 1, 1));
+        graph[2].add(new Edge(2, 3, 1));
+        graph[2].add(new Edge(2, 4, 4));
+        // graph[3].add(new Edge(0,1,5));
+        graph[3].add(new Edge(3, 1, 3));
+        graph[3].add(new Edge(3, 2, 1));
+        graph[4].add(new Edge(4, 1, 1));
+
+        bfs(graph);
+    }
+}
