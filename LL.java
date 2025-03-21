c2a7506817fa99e47ae54d3d514c4249c14b7941
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-03-21 20:55:40 +0530

    Updated

--- LL.java
+++ LL.java
@@ -1,46 +1,44 @@
-class ListNode {
-    int val;
-    ListNode next;
+class ListNode {     
+    int val;     
+    ListNode next;      
 
-    ListNode(int x) {
-        val = x;
-        next = null;
-    }
-}
+    ListNode(int x) {         
+        val = x;         
+        next = null;     
+    } 
+}  
 
-class LL {
-    public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode current = head;
+class LL {     
+    public ListNode reverseList(ListNode head) {         
+        ListNode prev = null;         
+        ListNode current = head;          
 
-        while (current != null) {
-            ListNode next = current.next; // Store next node
-            current.next = prev;  // Reverse the link
-            prev = current;  // Move prev forward
-            current = next;  // Move current forward
-        }
-        return prev;
-    }
-}
+        while (current != null) {             
+            ListNode next = current.next; // Store next node             
+            current.next = prev;  // Reverse the link             
+            prev = current;  // Move prev forward             
+            current = next;  // Move current forward         
+        }         
+        return prev;     
+    }      
 
-public class LL { // Added a class to contain main method
-    public static void main(String[] args) {
-        // Creating the linked list: 1 -> 2 -> 3 -> 4 -> 5
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
+    public static void main(String[] args) {         
+        // Creating the linked list: 1 -> 2 -> 3 -> 4 -> 5         
+        ListNode head = new ListNode(1);         
+        head.next = new ListNode(2);         
+        head.next.next = new ListNode(3);         
+        head.next.next.next = new ListNode(4);         
+        head.next.next.next.next = new ListNode(5);          
 
-        LL solution = new LL();
+        LL solution = new LL();          
 
-        // Reversing the list
-        ListNode reversedHead = solution.reverseList(head);
+        // Reversing the list         
+        ListNode reversedHead = solution.reverseList(head);          
 
-        // Printing the reversed list
-        while (reversedHead != null) {
-            System.out.print(reversedHead.val + " ");
-            reversedHead = reversedHead.next;
-        }
-    }
+        // Printing the reversed list         
+        while (reversedHead != null) {             
+            System.out.print(reversedHead.val + " ");             
+            reversedHead = reversedHead.next;         
+        }     
+    } 
 }
