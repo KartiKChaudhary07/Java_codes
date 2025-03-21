4ee3b60e45796c369cc9d73907ed31871109633e
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-03-21 20:57:57 +0530

    Updated

--- Main1.java
+++ Main1.java
@@ -1,9 +1,10 @@
 import java.util.*;
-public static int Calculatesum(int num1,int num2){
-    int sum=int num1+int num2;
-    return sum;
-}
+
 class Main1{
+    public static int Calculatesum(int num1,int num2){
+        int sum=num1+ num2;
+        return sum;
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a=sc.nextInt();
