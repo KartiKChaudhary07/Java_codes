8eeffbd29e213bb20da42e6be0a0b2648cddc227
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-03-21 20:50:56 +0530

    Stack IMplementation

---
+++ Stackpb.java
@@ -0,0 +1,51 @@
+public class Stackpb{
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static class stack{
+        static Node head=null;
+        public static boolean isEmpty(){
+            return head==null;
+        }
+        public static void push(int data){
+            Node newNode=new Node(data);
+            if(isEmpty()){
+                head=newNode;
+                return;
+            }
+            newNode.next=head;
+            head=newNode;
+        }
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top=head.data;
+            head=head.next;
+            return top;
+        }
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            return head.data;
+        }
+    }
+    public static void main(String []args){
+        stack s=new stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
