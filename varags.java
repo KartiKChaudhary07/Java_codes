ea716b34740f511763a3e2b31ad004023eca8d43
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-04-03 23:01:42 +0530

    remove errors

--- varags.java
+++ varags.java
@@ -1,21 +1,15 @@
-class varags{
-    public static void main(String[] args){
-        static int sum(int ...arr){
-            int result=0;
-            for(int a:arr){
-                result+=a;
-
-            }
-            return result;
-
+class Varargs {
+    static int sum(int... arr) {
+        int result = 0;
+        for (int a : arr) {
+            result += a;
         }
-        public static void main(String[] args) {
-            System.out.println("Wrlcome to varags methods:-");
-            System.out.println("The sum of 4 and 5 is"+ sum(...arr:4,5));
-            System.out.println("The sum of 3,6 and 7 is"+ sum(...arr:3,6,7));
-            
+        return result;
+    }
 
-            
-        }
+    public static void main(String[] args) {
+        System.out.println("Welcome to varargs methods:-");
+        System.out.println("The sum of 4 and 5 is " + sum(4, 5));
+        System.out.println("The sum of 3, 6, and 7 is " + sum(3, 6, 7));
     }
 }
