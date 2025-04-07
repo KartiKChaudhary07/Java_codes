bbe6f4e6c209c0d28874eaffd57833749af4116d
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-04-07 06:59:07 +0530

    Calendar.java

--- calendar22.java
+++ calendar22.java
@@ -1,12 +1,11 @@
 import java.time.LocalDateTime;
-import java.format.DateTimeFormatter;
-class calendar22{
-    public static void main(String[] args){
-        LocalDateTime dt=LocalDateTime.now();
-        
+import java.time.format.DateTimeFormatter;
 
-        DateTimeFormatter df= DateTimeFormatter.ISO_LOCAL_DATE;
-        String myDate=dt.fromat(df);
+class calendar22 {
+    public static void main(String[] args) {
+        LocalDateTime dt = LocalDateTime.now();
+        DateTimeFormatter df = DateTimeFormatter.ISO_LOCAL_DATE;
+        String myDate = dt.format(df); // Corrected method name: format instead of fromat
         System.out.println(myDate);
     }
 }
