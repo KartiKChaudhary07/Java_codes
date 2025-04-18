df3738e14d97eaeb4bbeed42ef20e97bfef433a6
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-04-18 14:04:12 +0530

    Initial commit

---
+++ isSorted.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class isSorted {
+    static boolean sorted(int arr[],int n){
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<arr[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(sorted(arr,n));
+    }
+}
