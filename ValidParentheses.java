58f57bda3a7990cad08690b3d3148e4b5541ca43
Kartik Chaudhary <kartik9983chaudhary@gmail.com>
2025-12-19 22:03:09 +0530

    java code

---
+++ ValidParentheses.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c) return false;
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()[]{}"));
+    }
+}
