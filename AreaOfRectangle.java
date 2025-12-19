6bd0fcb8bd21e706a13c75f96abb0897f5a3e17c
Kartik Chaudhary <kartik9983chaudhary@gmail.com>
2025-12-19 22:04:04 +0530

    Add files via upload

---
+++ AreaOfRectangle.java
@@ -0,0 +1,10 @@
+class PerimeterOfRectangle {
+    public static void main(String[] args) {
+        double length = 10;
+        double breadth = 5;
+
+        double perimeter = 2 * (length + breadth);
+
+        System.out.println("Perimeter of Rectangle = " + perimeter);
+    }
+}
