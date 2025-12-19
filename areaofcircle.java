058f00f901ac6c360f416f76f0e7971ff8abdc47
Kartik Chaudhary <kartik9983chaudhary@gmail.com>
2025-12-19 22:03:41 +0530

    java code

---
+++ areaofcircle.java
@@ -0,0 +1,8 @@
+class areaoficrcle {
+    public static void main(String[] args) {
+        double radius = 7;
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Area of Circle = " + area);
+    }
+}
