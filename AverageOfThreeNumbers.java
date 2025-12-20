7d85c22f672db2f3bb1cf24c8a93e7ad6a078874
Kartik Chaudhary <kartik9983chaudhary@gmail.com>
2025-12-20 10:57:29 +0530

    Add files via upload

---
+++ AverageOfThreeNumbers.java
@@ -0,0 +1,11 @@
+class AverageOfThreeNumbers {
+    public static void main(String[] args) {
+        double a = 10;
+        double b = 20;
+        double c = 30;
+
+        double avg = (a + b + c) / 3;
+
+        System.out.println("Average = " + avg);
+    }
+}
