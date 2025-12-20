583223d3a4fff8bdded031c88cbb8eb83bf4fc89
Kartik Chaudhary <kartik9983chaudhary@gmail.com>
2025-12-20 10:58:15 +0530

    Add files via upload

---
+++ AveragePCM.java
@@ -0,0 +1,11 @@
+class AveragePCM {
+    public static void main(String[] args) {
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        double average = (maths + physics + chemistry) / 3.0;
+
+        System.out.println("Sam's average mark in PCM is " + average);
+    }
+}
