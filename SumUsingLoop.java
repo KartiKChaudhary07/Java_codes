60a9a2ebfbe9f1d02ca2b3941acc5f9d6c853718
Kartik Chaudhary <kartik9983chaudhary@gmail.com>
2025-12-22 00:25:14 +0530

    Add files via upload

---
+++ SumUsingLoop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class SumCompareFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) sum += i;
+
+        System.out.println(sum + " " + (n * (n + 1) / 2));
+        sc.close();
+        
+    }
+}
