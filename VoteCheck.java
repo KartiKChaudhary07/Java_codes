6f1d48b9aa14661de11c18b1372b1bf7fd7bc471
Kartik Chaudhary <kartik9983chaudhary@gmail.com>
2025-12-22 00:24:54 +0530

    Add files via upload

---
+++ VoteCheck.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class VoteCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if (age >= 18)
+            System.out.println(age);
+        else
+            System.out.println(age);
+        sc.close();
+    }
+}
