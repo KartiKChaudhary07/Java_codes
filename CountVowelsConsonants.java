6f458a1a9c16d93b4a834a598d68988a2aa8f67d
Kartik Chaudhary <kartik9983chaudhary@gmail.com>
2025-12-26 16:32:44 +0530

    count Vowels

---
+++ CountVowelsConsonants.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class CountVowelsConsonants {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine().toLowerCase();
+
+        int vowels = 0, consonants = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+                    vowels++;
+                else
+                    consonants++;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        sc.close();
+    }
+}
